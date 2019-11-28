244388f31cb4d58671b31831d0c57a979f04e6cf
JareBear <jaredgrozz@gmail.com>
2019-11-28 06:47:04 -0800

    Update main.py

--- main.py
+++ main.py
@@ -452,6 +452,32 @@ class MainMenu(QWidget):
 
             total_batches += 5
 
+        with open(settings_dir + 'saved_batches.json') as file:
+            saved_batches_data = json.load(file)
+            batch_name.clear()
+            batch_thickness.clear()
+            batch_cutting_checked.clear()
+            batch_picking_checked.clear()
+            batch_bending_checked.clear()
+            batch_assembly_checked.clear()
+            batch_painting_checked.clear()
+            for info in saved_batches_data:
+                for name in info['name']:
+                    batch_name.append(name)
+                for path in info['path']:
+                    batch_path.append(path)
+                for cut_checked in info['cutting checked']:
+                    batch_cutting_checked.append(cut_checked)
+                for pick_checked in info['picking checked']:
+                    batch_picking_checked.append(pick_checked)
+                for bend_checked in info['bending checked']:
+                    batch_bending_checked.append(bend_checked)
+                for assemble_checked in info['assembly checked']:
+                    batch_assembly_checked.append(assemble_checked)
+                for paint_checked in info['painting checked']:
+                    batch_painting_checked.append(paint_checked)
+                for thickness in info['thickness']:
+                    batch_thickness.append(thickness)
         self.lblProgress.setText(str(unfinished_batches) + '/' + str(total_batches))
         self.progressbar.setValue(unfinished_batches)
         self.progressbar.setMaximum(total_batches)
@@ -488,9 +514,35 @@ class MainMenu(QWidget):
                     batch_thickness.append(thickness)
         self.clearLayout(self.lay)
         self.update_batches()
-    def delete_batch(self, bp, i):
-        self.index = i
+    def delete_batch(self, bp, ind):
+        self.index = ind
         global total_batches, unfinished_batches, saved_batches_data, batch_name, batch_thickness, batch_cutting_checked, batch_picking_checked, batch_bending_checked, batch_assembly_checked, batch_painting_checked, batch_path
+        with open(settings_dir + 'saved_batches.json') as file:
+            saved_batches_data = json.load(file)
+            batch_name.clear()
+            batch_thickness.clear()
+            batch_cutting_checked.clear()
+            batch_picking_checked.clear()
+            batch_bending_checked.clear()
+            batch_assembly_checked.clear()
+            batch_painting_checked.clear()
+            for info in saved_batches_data:
+                for name in info['name']:
+                    batch_name.append(name)
+                for path in info['path']:
+                    batch_path.append(path)
+                for cut_checked in info['cutting checked']:
+                    batch_cutting_checked.append(cut_checked)
+                for pick_checked in info['picking checked']:
+                    batch_picking_checked.append(pick_checked)
+                for bend_checked in info['bending checked']:
+                    batch_bending_checked.append(bend_checked)
+                for assemble_checked in info['assembly checked']:
+                    batch_assembly_checked.append(assemble_checked)
+                for paint_checked in info['painting checked']:
+                    batch_painting_checked.append(paint_checked)
+                for thickness in info['thickness']:
+                    batch_thickness.append(thickness)
         for i, j in enumerate(saved_batches_data):
             if self.index == i:
                 if j['path'][0] == bp:
@@ -934,90 +986,118 @@ class MainMenu(QWidget):
                     selected_files.clear()
                     selected_paths.clear()
     def add_batch_list(self, batches_file, batches_path):
-        global total_batches, unfinished_batches, saved_batches_data, batch_name, batch_thickness, batch_cutting_checked, batch_picking_checked, batch_bending_checked, batch_assembly_checked, batch_painting_checked, batch_path
+        global total_batches, unfinished_batches, saved_batches_data, batch_name, batch_thickness, batch_cutting_checked, batch_picking_checked, batch_bending_checked, batch_assembly_checked, batch_painting_checked, batch_path, paths_list
         source_index = self.proxy_model.mapToSource(self.index)
         indexItem = self.model.index(source_index.row(), 0, source_index.parent())
         self.fileName = self.model.fileName(indexItem)
         self.filePath = self.model.filePath(indexItem)
         self.listPath = batches_path
-        # print(self.listPath)
         self.listFile = batches_file
-        # print(self.listFile)
         if not total_batches == 0 :
             try:
                 perc = int(unfinished_batches / total_batches * 100)
                 self.setWindowTitle(self.filePath + '  ' + str(perc) + '%')
             except Exception as DivisionByZero:
                 self.setWindowTitle(self.filePath + '  ' + str(0) + '%')
-        
+
         if os.path.exists(settings_dir + 'saved_batches.json'):
+            with open(settings_dir + 'saved_batches.json') as file:
+                saved_batches_data = json.load(file)
             for i, j in enumerate(self.listFile):
-                with open(settings_dir + 'saved_batches.json') as file:
-                    saved_batches_data = json.load(file)
-                p = paths_list[i]
-                # p = p.replace('\\', '/')
-                # p = p.split('/')
-                # p[0] = p[0].capitilize()
-                # p = '/'.join(p)
-                if p == self.listPath[i]:
-                    self.mt = metal_thickness_list[i]
-                # for i, j in enumerate(paths_list):
-                #     j = j.replace('\\', '/')
-                #     j = j.split('/')
-                #     j[0] = j[0].capitalize()
-                #     j = '/'.join(j)
-                #     if self.filePath == j:
-                #         self.mt = metal_thickness_list[i]
-                # batch_list.append(self.fileName)
-                saved_batches_data.append({
-                'cutting checked': ['False'],
-                'picking checked': ['False'],
-                'bending checked': ['False'],
-                'assembly checked': ['False'],
-                'painting checked': ['False'],
-                'path': [self.listPath[i]],
-                'name': [j],
-                'thickness': [self.mt.replace(' Gauge', '')],
-                })
-                file_copy_location = folder + '/'
-                if not os.path.exists(file_copy_location):
-                    os.makedirs(file_copy_location)
-                # sort json file
-                sorted_saved_batches_data = sorted(saved_batches_data, key=itemgetter('thickness'), reverse=True)
-                with open(settings_dir + 'saved_batches.json', mode='w+', encoding='utf-8') as file:
-                    json.dump(sorted_saved_batches_data, file, ensure_ascii=True, indent=4, sort_keys=True)
-                with open(settings_dir + 'saved_batches.json') as file:
-                    saved_batches_data = json.load(file)
-                    batch_name.clear()
-                    batch_thickness.clear()
-                    batch_cutting_checked.clear()
-                    batch_picking_checked.clear()
-                    batch_bending_checked.clear()
-                    batch_assembly_checked.clear()
-                    batch_painting_checked.clear()
-                    for info in saved_batches_data:
-                        for name in info['name']:
-                            batch_name.append(name)
-                        for path in info['path']:
-                            batch_path.append(path)
-                        for cut_checked in info['cutting checked']:
-                            batch_cutting_checked.append(cut_checked)
-                        for pick_checked in info['picking checked']:
-                            batch_picking_checked.append(pick_checked)
-                        for bend_checked in info['bending checked']:
-                            batch_bending_checked.append(bend_checked)
-                        for assemble_checked in info['assembly checked']:
-                            batch_assembly_checked.append(assemble_checked)
-                        for paint_checked in info['painting checked']:
-                            batch_painting_checked.append(paint_checked)
-                        for thickness in info['thickness']:
-                            batch_thickness.append(thickness)
+                    p = paths_list[i]
+                    p = p.replace('\\', '/')
+                    p = p.split('/')
+                    p[0] = p[0].capitalize()
+                    p = '/'.join(p)
+
+                    o = self.listPath[i]
+                    o = o.replace('\\', '/')
+                    o = o.split('/')
+                    o[0] = o[0].capitalize()
+                    o = '/'.join(o)
+                    if o == p:
+                        self.mt = metal_thickness_list[i]
+                    # for i, j in enumerate(paths_list):
+                    #     j = j.replace('\\', '/')
+                    #     j = j.split('/')
+                    #     j[0] = j[0].capitalize()
+                    #     j = '/'.join(j)
+                    #     if self.filePath == j:
+                    #         self.mt = metal_thickness_list[i]
+                    # batch_list.append(self.fileName)
+                    saved_batches_data.append({
+                    'cutting checked': ['False'],
+                    'picking checked': ['False'],
+                    'bending checked': ['False'],
+                    'assembly checked': ['False'],
+                    'painting checked': ['False'],
+                    'path': [self.listPath[i]],
+                    'name': [j],
+                    'thickness': [self.mt.replace(' Gauge', '')],
+                    })
+                    file_copy_location = folder + '/'
+                    if not os.path.exists(file_copy_location):
+                        os.makedirs(file_copy_location)
+                    # sort json file
+                    sorted_saved_batches_data = sorted(saved_batches_data, key=itemgetter('thickness'), reverse=True)
+                    with open(settings_dir + 'saved_batches.json', mode='w+', encoding='utf-8') as file:
+                        json.dump(sorted_saved_batches_data, file, ensure_ascii=True, indent=4, sort_keys=True)
+                    with open(settings_dir + 'saved_batches.json') as file:
+                        saved_batches_data = json.load(file)
+                        batch_name.clear()
+                        batch_thickness.clear()
+                        batch_cutting_checked.clear()
+                        batch_picking_checked.clear()
+                        batch_bending_checked.clear()
+                        batch_assembly_checked.clear()
+                        batch_painting_checked.clear()
+                        for info in saved_batches_data:
+                            for name in info['name']:
+                                batch_name.append(name)
+                            for path in info['path']:
+                                batch_path.append(path)
+                            for cut_checked in info['cutting checked']:
+                                batch_cutting_checked.append(cut_checked)
+                            for pick_checked in info['picking checked']:
+                                batch_picking_checked.append(pick_checked)
+                            for bend_checked in info['bending checked']:
+                                batch_bending_checked.append(bend_checked)
+                            for assemble_checked in info['assembly checked']:
+                                batch_assembly_checked.append(assemble_checked)
+                            for paint_checked in info['painting checked']:
+                                batch_painting_checked.append(paint_checked)
+                            for thickness in info['thickness']:
+                                batch_thickness.append(thickness)
         elif not os.path.exists(settings_dir + 'saved_batches.json'):
             file = open(settings_dir + "saved_batches.json", "w+")
             file.write("[]")
             file.close()
         with open(settings_dir + 'saved_batches.json') as file:
             saved_batches_data = json.load(file)
+            batch_name.clear()
+            batch_thickness.clear()
+            batch_cutting_checked.clear()
+            batch_picking_checked.clear()
+            batch_bending_checked.clear()
+            batch_assembly_checked.clear()
+            batch_painting_checked.clear()
+            for info in saved_batches_data:
+                for name in info['name']:
+                    batch_name.append(name)
+                for path in info['path']:
+                    batch_path.append(path)
+                for cut_checked in info['cutting checked']:
+                    batch_cutting_checked.append(cut_checked)
+                for pick_checked in info['picking checked']:
+                    batch_picking_checked.append(pick_checked)
+                for bend_checked in info['bending checked']:
+                    batch_bending_checked.append(bend_checked)
+                for assemble_checked in info['assembly checked']:
+                    batch_assembly_checked.append(assemble_checked)
+                for paint_checked in info['painting checked']:
+                    batch_painting_checked.append(paint_checked)
+                for thickness in info['thickness']:
+                    batch_thickness.append(thickness)
         self.clearLayout(self.lay)
         self.update_batches()
         # TREE VIEW END ====================================
@@ -1030,6 +1110,33 @@ class MainMenu(QWidget):
                     widget.deleteLater()
                 else:
                     self.clearLayout(item.layout())
+
+        with open(settings_dir + 'saved_batches.json') as file:
+            saved_batches_data = json.load(file)
+            batch_name.clear()
+            batch_thickness.clear()
+            batch_cutting_checked.clear()
+            batch_picking_checked.clear()
+            batch_bending_checked.clear()
+            batch_assembly_checked.clear()
+            batch_painting_checked.clear()
+            for info in saved_batches_data:
+                for name in info['name']:
+                    batch_name.append(name)
+                for path in info['path']:
+                    batch_path.append(path)
+                for cut_checked in info['cutting checked']:
+                    batch_cutting_checked.append(cut_checked)
+                for pick_checked in info['picking checked']:
+                    batch_picking_checked.append(pick_checked)
+                for bend_checked in info['bending checked']:
+                    batch_bending_checked.append(bend_checked)
+                for assemble_checked in info['assembly checked']:
+                    batch_assembly_checked.append(assemble_checked)
+                for paint_checked in info['painting checked']:
+                    batch_painting_checked.append(paint_checked)
+                for thickness in info['thickness']:
+                    batch_thickness.append(thickness)
     def import_pdf(self):
         global saved_data, paths_list, names_list, folder_list, metal_thickness_list, metal_type_list, cut_time_list, bend_time_list, weight_list
         with open(settings_dir + 'saved_data.json') as file:
@@ -1713,8 +1820,6 @@ class view_details(QWidget):
                 m[0] = m[0].capitalize()
                 m = '/'.join(m)
                 self.pdfText.setPlainText(f"""File Name: {j}\nFile Path: {m}\n\nDestination: {n}\n\nSelected Metal Thickness: {metal_thickness_list[i]}\nSelected Metal Type: {metal_type_list[i]}\n\nCut Time: {cut_time_list[i]}\nBend Time: {bend_time_list[i]}\nWeight: {weight_list[i]}\n\nPrice: {self.price}""")
-            else:
-                self.pdfText.setPlainText(f"""Can't retrieve any data.""")
 
         self.thumbnail = QPushButton('View image', self)
         self.thumbnail.clicked.connect(self.openImage)
