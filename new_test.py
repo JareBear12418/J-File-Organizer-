698b482660fef3da6dc0f5515b92759e7b9a09be
JareBear <jaredgrozz@gmail.com>
2019-12-06 15:05:28 -0600

    Update new_test.py

--- new_test.py
+++ new_test.py
@@ -6,78 +6,120 @@ from PyQt5.QtGui import *
 from PyQt5 import *
 import os, sys
 class TreeDelegate(QtWidgets.QStyledItemDelegate):
-    buttonClicked = pyqtSignal(QModelIndex)
+    buttonClicked = pyqtSignal(QModelIndex, int)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, fsModel, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fsModel = fsModel
+
+        self.clickedPaths = {}
         self._mousePos = None
         self._pressed = False
-        self.buttonWidth = 32
-        self.buttonIcon = QIcon('some icon')
+        self.minimumButtonWidth = 32
+
+    def getOption(self, option, index):
+        btnOption = QStyleOptionButton()
+        # initialize the basic options with the view
+        btnOption.initFrom(option.widget)
+
+        clickedCount = self.clickedPaths.get(self.fsModel.filePath(index), 0)
+        if clickedCount == 0:
+            btnOption.text = 'Cutting'
+        elif clickedCount == 1:
+            btnOption.text = 'Picking'
+        elif clickedCount == 2:
+            btnOption.text = 'Bending'
+        elif clickedCount == 3:
+            btnOption.text = 'Painting'
+        elif clickedCount == 4:
+            btnOption.text = 'Assembly'
+        elif clickedCount >= 5:
+            btnOption.text = 'Complete'
+        else:
+            btnOption.text = 'Cutting'
+
+        # the original option properties should never be touched, so we can't
+        # directly use it's "rect"; let's create a new one from it
+        btnOption.rect = QRect(option.rect)
+
+        # adjust it to the minimum size
+        btnOption.rect.setLeft(option.rect.right() - self.minimumButtonWidth)
+
+        style = option.widget.style()
+        # get the available space for the contents of the button
+        textRect = style.subElementRect(
+            QStyle.SE_PushButtonContents, btnOption)
+        # get the margins between the contents and the border, multiplied by 2
+        # since they're used for both the left and right side
+        margin = style.pixelMetric(
+            QStyle.PM_ButtonMargin, btnOption) * 2
+
+        # the width of the current button text
+        textWidth = btnOption.fontMetrics.width(btnOption.text)
+
+        if textRect.width() < textWidth + margin:
+            # if the width is too small, adjust the *whole* button rect size
+            # to fit the contents
+            btnOption.rect.setLeft(btnOption.rect.left() - (
+                textWidth - textRect.width() + margin))
+
+        return btnOption
 
     def editorEvent(self, event, model, option, index):
-        if event.type() in (QEvent.Enter, QEvent.MouseMove):
-            self._mousePos = event.pos()
-            # request an update of the current index
-            option.widget.update(index)
-        elif event.type() == QEvent.Leave:
-            self._mousePos = None
-        elif event.type() == QEvent.MouseButtonPress and event.button() == Qt.LeftButton:
-            # check that the click is within the virtual button rectangle; note
-            # that the option rect shouldn't be touched, so we create a new one
-            # based on it
-            rect = QRect(option.rect)
-            rect.setLeft(rect.right() - self.buttonWidth)
-            if event.pos() in rect:
-                self._pressed = True
-            option.widget.update(index)
-        elif event.type() == QEvent.MouseButtonRelease:
-            if self._pressed and event.button() == Qt.LeftButton:
-                rect = QRect(option.rect)
-                rect.setLeft(rect.right() - self.buttonWidth)
-                # emit the click only if the release is within the button rect
-                if event.pos() in rect:
-                    self.buttonClicked.emit(index)
-            self._pressed = False
-            option.widget.update(index)
+        # map the proxy index to the fsModel
+        srcIndex = index.model().mapToSource(index)
+        # I'm just checking if it's a file, if you want to check the extension
+        # you might need to use fsModel.fileName(srcIndex)
+        if not self.fsModel.isDir(srcIndex):
+            if event.type() in (QEvent.Enter, QEvent.MouseMove):
+                self._mousePos = event.pos()
+                # request an update of the current index
+                option.widget.update(index)
+            elif event.type() == QEvent.Leave:
+                self._mousePos = None
+            elif (event.type() in (QEvent.MouseButtonPress, QEvent.MouseButtonDblClick)
+                and event.button() == Qt.LeftButton):
+                    # check that the click is within the virtual button rectangle
+                    if event.pos() in self.getOption(option, srcIndex).rect:
+                        self._pressed = True
+                    option.widget.update(index)
+                    if event.type() == QEvent.MouseButtonDblClick:
+                        # do not send double click events
+                        return True
+            elif event.type() == QEvent.MouseButtonRelease:
+                if self._pressed and event.button() == Qt.LeftButton:
+                    # emit the click only if the release is within the button rect
+                    if event.pos() in self.getOption(option, srcIndex).rect:
+                        filePath = self.fsModel.filePath(srcIndex)
+                        count = self.clickedPaths.setdefault(filePath, 0)
+                        self.buttonClicked.emit(index, count + 1)
+                        self.clickedPaths[filePath] += 1
+                self._pressed = False
+                option.widget.update(index)
         return super().editorEvent(event, model, option, index)
-
 
     def paint(self, painter, option, index):
         super().paint(painter, option, index)
-        if index.isValid():
-            proxy = index.model()
-            fsModel = proxy.sourceModel()
-            # map the proxy index to the fsModel
-            srcIndex = proxy.mapToSource(index)
-            # I'm just checking if it's a file, if you want to check the extension
-            # you might need to use fsModel.fileName(srcIndex)
-            if not fsModel.isDir(srcIndex):
-                btnOption = QStyleOptionButton()
-                btnOption.text = 'hi'
-                # initialize the basic options with the view
-                btnOption.initFrom(option.widget)
-                # you can also use fsModel.fileIcon(srcIndex)
-                btnOption.icon = self.buttonIcon
-                # as before, create a new rectangle
-                btnOption.rect = QRect(option.rect)
-                btnOption.rect.setLeft(option.rect.right() - self.buttonWidth)
-                # remove the focus rectangle, as it will be inherited from the view
-                btnOption.state &= ~QStyle.State_HasFocus
-                if self._mousePos and self._mousePos in btnOption.rect:
-                    # if the style supports it, some kind of "glowing" border
-                    # will be shown on the button
-                    btnOption.state |= QStyle.State_MouseOver
-                    if self._pressed == Qt.LeftButton:
-                        # set the button pressed state
-                        btnOption.state |= QStyle.State_On
-                else:
-                    # ensure that there's no mouse over state (see above)
-                    btnOption.state &= ~QStyle.State_MouseOver
+        srcIndex = index.model().mapToSource(index)
+        if not self.fsModel.isDir(srcIndex):
+            btnOption = self.getOption(option, srcIndex)
 
-                # finally, draw the virtual button
-                option.widget.style().drawControl(QStyle.CE_PushButton, btnOption, painter)
+            # remove the focus rectangle, as it will be inherited from the view
+            btnOption.state &= ~QStyle.State_HasFocus
+            if self._mousePos is not None and self._mousePos in btnOption.rect:
+                # if the style supports it, some kind of "glowing" border
+                # will be shown on the button
+                btnOption.state |= QStyle.State_MouseOver
+                if self._pressed == Qt.LeftButton:
+                    # set the button pressed state
+                    btnOption.state |= QStyle.State_On
+            else:
+                # ensure that there's no mouse over state (see above)
+                btnOption.state &= ~QStyle.State_MouseOver
 
+            # finally, draw the virtual button
+            option.widget.style().drawControl(
+                QStyle.CE_PushButton, btnOption, painter)
 
 class MainMenu(QWidget):
     def __init__(self, parent = None):
@@ -105,7 +147,7 @@ class MainMenu(QWidget):
         self.treeView.setAcceptDrops(False)
         self.treeView.setDropIndicatorShown(True)
         self.treeView.setEditTriggers(QTreeView.NoEditTriggers)
-        self.treeDelegate = TreeDelegate()
+        self.treeDelegate = TreeDelegate(self.model)
         self.treeView.setItemDelegateForColumn(0, self.treeDelegate)
         # self.treeDelegate.setText('press me :)')
         self.treeDelegate.buttonClicked.connect(self.treeButtonClicked)
